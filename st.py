9f76b5aa8e80786ff484b5eb8a605bf0a448d26a
hqz-11 <106565583+hqz-11@users.noreply.github.com>
2022-11-01 11:20:25 +0900

    Add files via upload

--- st.py
+++ st.py
@@ -1,11 +1,32 @@
-import streamlit as st
-import pandas as pd
+#import streamlit as st
+#import pandas as pd
 #pip install openpyxl
+import streamlit as st
+from google.oauth2 import service_account
+from google.cloud import storage
 
+# Create API client.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+client = storage.Client(credentials=credentials)
+
+# Retrieve file contents.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def read_file(bucket_name, file_path):
+    bucket = client.bucket(bucket_name)
+    content = bucket.blob(file_path).download_as_string().decode("utf-8")
+    return content
+
+bucket_name = "streamlit-bucket"
+file_path = "myfile.csv"
+
+df = read_file(bucket_name, file_path)
 st.markdown("# keywordsraech")
 
 #input your xlsx path
-df = pd.read_excel("test.xlsx", engine='openpyxl')
+#df = pd.read_excel("/Users/qizhihan/Desktop/py2/test.xlsx", engine='openpyxl')
 
 #アイテムの選択を作る
 keyword= df['keyword'].unique().tolist()
