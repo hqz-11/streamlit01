efdfbd0426be1017eccd43df44dbf146e089f1de
hqz-11 <106565583+hqz-11@users.noreply.github.com>
2022-11-01 10:15:08 +0900

    Add files via upload

---
+++ st.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+#pip install openpyxl
+
+st.markdown("# keywordsraech")
+
+#input your xlsx path
+df = pd.read_excel("/Users/qizhihan/Desktop/py2/test.xlsx", engine='openpyxl')
+
+#アイテムの選択を作る
+keyword= df['keyword'].unique().tolist()
+mondai = df['問題'].unique().tolist()
+k_select = st.sidebar.selectbox("keywordを選択してください", keyword)
+m_select = st.sidebar.selectbox("問題を選択してください", mondai)
+result_df = df[(df['keyword'] == k_select) & (df['問題'] == m_select)]
+
+#回答を出力する
+if len(result_df) == 0:
+    st.write("答えはありません")
+else:
+    st.write("答えの例： ")
+    for i in range(len(result_df)):
+        st.write(result_df["答え1"].values[i])
+        st.write(result_df["答え2"].values[i])
+        st.write(result_df["答え3"].values[i])
