e0e92148049f22e9ad249d94ad14400826ca28f1
hqz-11 <106565583+hqz-11@users.noreply.github.com>
2022-11-01 12:19:01 +0900

    Update testcs.py

--- testcs.py
+++ testcs.py
@@ -21,9 +21,21 @@ def read_file(bucket_name, file_path):
 bucket_name = "streamlit-kann"
 file_path = "test.xlsx"
 
-content = read_file(bucket_name, file_path)
+df = read_file(bucket_name, file_path)
 
 # Print results.
-for line in content.strip().split("\n"):
-    keyword, 問題 = line.split(",")
-    st.write(f"{keyword} has a :{問題}:")
+keyword= df['keyword'].unique().tolist()
+mondai = df['問題'].unique().tolist()
+k_select = st.sidebar.selectbox("keywordを選択してください", keyword)
+m_select = st.sidebar.selectbox("問題を選択してください", mondai)
+result_df = df[(df['keyword'] == k_select) & (df['問題'] == m_select)]
+
+#回答を出力する
+if len(result_df) == 0:
+    st.write("答えはありません")
+else:
+    st.write("答えの例： ")
+    for i in range(len(result_df)):
+        st.write(result_df["答え1"].values[i])
+        st.write(result_df["答え2"].values[i])
+        st.write(result_df["答え3"].values[i])
