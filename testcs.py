8e1478845bbc6952fe97e4dbd1502d18b9490480
hqz-11 <106565583+hqz-11@users.noreply.github.com>
2022-11-01 12:26:26 +0900

    Update testcs.py

--- testcs.py
+++ testcs.py
@@ -24,8 +24,8 @@ file_path = "test.xlsx"
 df = read_file(bucket_name, file_path)
 
 # Print results.
-keyword= df['keyword']
-mondai = df['問題']
+keyword= df["keyword"]
+mondai = df["問題"]
 k_select = st.sidebar.selectbox("keywordを選択してください", keyword)
 m_select = st.sidebar.selectbox("問題を選択してください", mondai)
 result_df = df[(df['keyword'] == k_select) & (df['問題'] == m_select)]
