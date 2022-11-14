59610e72e30a6241f6c10b3b6f43a417694dfe55
hqz-11 <106565583+hqz-11@users.noreply.github.com>
2022-11-14 15:45:27 +0900

    Update testgs4.py

--- testgs4.py
+++ testgs4.py
@@ -92,7 +92,7 @@ if check_password():
         x1=len(list)
         x2=x1+1
         st.title(x1)
-        st.metric(value="a"+x2)
+        st.metric(label="#a:",value="a")
         
             
     
