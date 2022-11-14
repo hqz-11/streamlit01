718d321bca4bf4176f0b11c99480bead08f947e0
hqz-11 <106565583+hqz-11@users.noreply.github.com>
2022-11-14 15:17:06 +0900

    Update testgs4.py

--- testgs4.py
+++ testgs4.py
@@ -86,9 +86,10 @@ if check_password():
                 st.write(result_df["答え2"].values[i])
                 st.write(result_df["答え3"].values[i]) 
     elif c:
-        len(worksheet_list)	
         SP_SHEET = 'Sheet3' 
         worksheet = sh.worksheet(SP_SHEET)
-        add1 = worksheet.update_cell(17,1,"田中")      
+        worksheet_list = workbook.worksheets()
+        len(worksheet_list)	
+            
     
     
