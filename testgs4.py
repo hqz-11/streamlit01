ae96c98b0cec3af52674d43aafd73d72a388add5
hqz-11 <106565583+hqz-11@users.noreply.github.com>
2022-11-14 16:03:09 +0900

    Update testgs4.py

--- testgs4.py
+++ testgs4.py
@@ -95,7 +95,7 @@ if check_password():
         title2 = st.text_input("内容","内容")
         d=st.button("update")
         if d:
-            worksheet.update_cell(x2, title1, title2)
+            worksheet.update_cell(x2, 1, title2)
         
             
     
