717ab7b73076452087e6a7ac86285aebf1af6344
hqz-11 <106565583+hqz-11@users.noreply.github.com>
2022-11-15 10:30:29 +0900

    Update update.py

--- update.py
+++ update.py
@@ -61,7 +61,7 @@ if check_password():
         update3 = worksheet.update_cell(x2,3,title3)
         update4 = worksheet.update_cell(x2,4,title4)
         update5 = worksheet.update_cell(x2,5,title5)
-    if st.butto("cancel"):
+    if st.button("cancel"):
         cancel = worksheet.delete_row(x2)
        
     
