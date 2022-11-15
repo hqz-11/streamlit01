698ff0a69a9d0024f2c00fb9edaa9591c9ece3c1
hqz-11 <106565583+hqz-11@users.noreply.github.com>
2022-11-15 11:10:40 +0900

    Update update.py

--- update.py
+++ update.py
@@ -51,7 +51,7 @@ if check_password():
     x2=x1+1
     st.markdown("# sheet update")
     st.title("enterを押してください")
-    ik = worksheet.acell(x1).value
+    ik = worksheet.col_values(x1)
     st.write(ik)
     title1 = st.text_input("keyword")
     title2 = st.text_input("問題")
