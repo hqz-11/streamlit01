1b0ee1b1340a048f8139a37486198bf9c8ae665f
hqz-11 <106565583+hqz-11@users.noreply.github.com>
2022-11-15 12:06:14 +0900

    Update update.py

--- update.py
+++ update.py
@@ -51,7 +51,7 @@ if check_password():
     worksheet = sh.worksheet(SP_SHEET)
     
     if st.button("sheet check"):
-        st.write(worksheet.get_all_values())
+        st.dataframe(worksheet.get_all_values())
     
     x1=len(worksheet.get_all_values())
     x2=x1+1
