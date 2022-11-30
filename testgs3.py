6294af9e89a5b18583fb66f4058de1ecc1b64937
hqz-11 <106565583+hqz-11@users.noreply.github.com>
2022-11-30 16:07:47 +0900

    Update testgs3.py

--- testgs3.py
+++ testgs3.py
@@ -25,7 +25,7 @@ def check_password():
     if "password_correct" not in st.session_state:
         # First run, show input for password.
         st.text_input(
-            "Password", type="password", on_change=password_entered, key="password"
+            "パスワードを入れてください", type="password", on_change=password_entered, key="password"
         )
         return False
     elif not st.session_state["password_correct"]:
