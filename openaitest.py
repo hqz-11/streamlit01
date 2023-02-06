608dd99abc6ce0d452a4495ad25cb6dd4bd8dc06
hqz-11 <106565583+hqz-11@users.noreply.github.com>
2023-02-06 18:56:44 +0900

    Add files via upload

---
+++ openaitest.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import openai
+import os
+
+# Set the API key
+openai.api_key = ("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")#鍵はopenaiの会員登録で入手する必要があります。
+
+# Create a text input widget
+question = st.text_input("Ask a question:")
+
+# Call the OpenAI API to get the answer
+completion = openai.Completion.create(
+    engine="text-davinci-002",
+    prompt=question,
+    max_tokens=1024,
+    temperature=0.5,
+)
+
+# Get the answer from the API response
+answer = completion.choices[0].text
+
+# Display the answer in a text output widget
+st.text(answer)
